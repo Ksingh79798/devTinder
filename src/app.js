fd8d9ce6d81eb594384bece37062b83e01daaee5
Ksingh79798 <118569392+Ksingh79798@users.noreply.github.com>
2024-10-01 20:55:18 +0530

    Ep-4 part-4 Regular Expression in Routes

--- src/app.js
+++ src/app.js
@@ -2,29 +2,58 @@
 const express = require("express");
 const app = express();
 
-// All routes are matching over here so, below all the routes never get a chance to execute
-// app.use("/", (req, res) => {
-//   res.send("HAAAAAAAAAAAAAA");
-// });
-
-
-// This will only handle GET call to  /user
-app.get("/user", (req, res) => {
+app.get("/abc", (req, res) => {
   res.send({ fname: "Vicky", lname: "Kumar" });
 });
 
-app.post("/user", (req, res) => {
-  // Saving Data to DB
-  res.send("data successfully saved to the DB");
+app.get("/ab?c", (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted Successfully");
+app.get("/ab+c", (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
 });
 
-// This will match all the HTTP Methods API Calls to  /hello
-app.use("/hello", (req, res) => {
-  res.send("Hello, Hello, Hello");
+app.get("ab*cd", (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+// Also do group together
+app.get("/a(bc)?d", (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+app.get("/a(bc)+d", (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+// Also write Regrex over here
+app.get(/a/, (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+app.get(/.*fly$/, (req, res) => {
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+// For fetch/get query param in my controller/route handlers we use req.query,
+//write in postman as:- Base_Url/user?userId=101  or Base_Url/user?userId=101&password=testing
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+//  : means Dynamic Routes, for fetch D.R we use req.params
+// Base_url/user/707
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ fname: "Vicky", lname: "Kumar" });
+});
+
+// Base_url/user/707/vicky/testing
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ fname: "Vicky", lname: "Kumar" });
 });
 
 app.listen(3000, () => {
