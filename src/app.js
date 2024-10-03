4cc64387cbd9949145ad9aa437f9f8030e39eaac
Ksingh79798 <118569392+Ksingh79798@users.noreply.github.com>
2024-10-03 15:26:05 +0530

    Ep-05 part-1 Multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,60 +1,215 @@
+// Ep-05 Middlewares & Error Handlers  Part-01
 // Create a Server
 const express = require("express");
 const app = express();
 
-app.get("/abc", (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
+// Ex-1:-It goes in infinite-loop in Postman/Client/Server as [Sending request...]
+app.use("/user", (req, res) => {});
+
+// Ex-2:-It also goes in infinite-loop in Postman/Client as [Sending request...]
+app.use("/user", (req, res) => {
+  console.log("Handling the route user1");
 });
 
-app.get("/ab?c", (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
+// Ex-3:- It return in Postman as [Responding1] & In console "Handling the route user1"
+app.use("/user", (req, res) => {
+  console.log("Handling the route user1");
+  res.send("Responding1");
 });
 
-app.get("/ab+c", (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-4:-It return in Postman as [Responding1] & In console "Handling the route user1"
+app.use(
+  "/user",
+  (req, res) => {
+    console.log("Handling the route user1");
+    res.send("Responding1");
+  },
+  (req, res) => {
+    console.log("Handling the route user2");
+    res.send("Responding2");
+  }
+);
 
-app.get("ab*cd", (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-4.1:-It goes in infinite-loop in Postman/Client as [Sending request...]
+app.use(
+  "/user",
+  (req, res) => {
+    console.log("Handling the route user1");
+    // res.send("Responding1");
+  },
+  (req, res) => {
+    console.log("Handling the route user2");
+    res.send("Responding2");
+  }
+);
 
-// Also do group together
-app.get("/a(bc)?d", (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-5:-It return in console [1,2,Error]  & In Postman [Res1]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    res.send("Res1");
+    next(); /*now called next rH here*/
+  },
+  (req, res) => {
+    console.log("2");
+    res.send(
+      "Res2"
+    ); /* get Error here bcz we don't send again another response to client/server in same path(/user) */
+  }
+);
 
-app.get("/a(bc)+d", (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-5.1:-It return in console [1,2]  & In Postman [Res2]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    // res.send("Res1");
+    next();
+  },
+  (req, res) => {
+    console.log("2");
+    res.send("Res2");
+  }
+);
 
-// Also write Regrex over here
-app.get(/a/, (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-6:-It return in console [1,2,Error]  & In Postman [Res2]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    next();
+    res.send(
+      "Res1"
+    ); /*get Error here bcz allready send response(Res2) to client/server */
+  },
+  (req, res) => {
+    console.log("2");
+    res.send("Res2");
+  }
+);
 
-app.get(/.*fly$/, (req, res) => {
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-7:-It return in console [1,2]  & In Postman [Res2]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    // res.send("Res1");
+    next();
+  },
+  (req, res) => {
+    console.log("2");
+    res.send("Res2");
+  },
+  // below all these rH will never execute/called bcz they can only be called if next() fn was called
+  (req, res) => {
+    console.log("3");
+    res.send("Res3");
+  },
+  (req, res) => {
+    console.log("4");
+    res.send("Res4");
+  },
+  (req, res) => {
+    console.log("5");
+    res.send("Res5");
+  }
+);
 
-// For fetch/get query param in my controller/route handlers we use req.query,
-//write in postman as:- Base_Url/user?userId=101  or Base_Url/user?userId=101&password=testing
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-8:-It return in console [1,2,3,4,5]  & In Postman [Res5]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    // res.send("Res1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2");
+    // res.send("Res2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3");
+    // res.send("Res3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4");
+    //res.send("Res4");
+    next();
+  },
+  (req, res) => {
+    console.log("5");
+    res.send("Res5");
+  }
+);
 
-//  : means Dynamic Routes, for fetch D.R we use req.params
-// Base_url/user/707
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-8.1:-It return in console [1,2,3,4,5,Error]  & In Postman [Res5]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    // res.send("Res1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2");
+    // res.send("Res2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3");
+    // res.send("Res3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4");
+    //res.send("Res4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("5");
+    res.send("Res5");
+    next(); /*get Error: [cannot GET /user] bcz after this line we don't defined any rH */
+  }
+);
 
-// Base_url/user/707/vicky/testing
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ fname: "Vicky", lname: "Kumar" });
-});
+// Ex-8.2:-If last next() fn is comment then It goes in infinite-loop in Postman/Client as [Sending request...]
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1");
+    // res.send("Res1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2");
+    // res.send("Res2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3");
+    // res.send("Res3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4");
+    //res.send("Res4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("5");
+    res.send("Res5");
+    // next();
+  }
+);
+
+// Also Wrapping these route handlers(Fn) indise Arrays
+// app.use("/route", [rh1, rH2, rH3, rH4, rH5]);  or
+// app.use("/route", [rh1, rH2], rH3, rH4, rH5);  or
+// app.use("/route", rh1, [rH2], rH3, rH4, rH5);  or
+// app.use("/route", rh1, [rH2, rH3], rH4, rH5);
 
 app.listen(3000, () => {
   console.log("Server is successfully started listening on port 3000....");
--- src/homework.md
+++ src/homework.md
@@ -1,34 +1,45 @@
         Episode-3
 
--Create a gitHub Repository
--Initialize the Repository
--Diff b/w node_modules, package.json, package_lock.json
--install express
--create a server
--Listen to port 3000
--Write request handlers for /hello, /test
--Install nodemon & Update scripts inside package.json
--what are Dependencies?
--Diff b/w carret(^) and tilde(~)
--What is the use of "-g" while npm install
+- Create a gitHub Repository
+- Initialize the Repository
+- Diff b/w node_modules, package.json, package_lock.json
+- install express
+- create a server
+- Listen to port 3000
+- Write request handlers for /hello, /test
+- Install nodemon & Update scripts inside package.json
+- what are Dependencies?
+- Diff b/w carret(^) and tilde(~)
+- What is the use of "-g" while npm install
 
 Ep-4 :- Routing And Request Handlers
 
           part-01
--Initialize a git
--create a .gitignore file
--create a remote repo on github
+
+- Initialize a git
+- create a .gitignore file
+- create a remote repo on github
 
           part-02
--push all code to remote origin
--play with routes & route extension Ex:- /, /hello, /hello/2, /test, /xyz, etc
+
+- push all code to remote origin
+- play with routes & route extension Ex:- /, /hello, /hello/2, /test, /xyz, etc
 
           part-03
--install postman app & make a workspace/collection -> Test API call
--write logic to handle GET, POST, PATCH, DELETE, API calls & test them on postman
+
+- install postman app & make a workspace/collection -> Test API call
+- write logic to handle GET, POST, PATCH, DELETE, API calls & test them on postman
 
           part-04
--Explore different types of routing & use of ?, +, (), * in the routes
--use of Regex in routes as /a/, /.*fly$/
--How to read query params in the routes- req.query
--How to read dynamics routes- req.params
+
+- Explore different types of routing & use of ?, +, (), \* in the routes
+- use of Regex in routes as /a/, /.\*fly$/
+- How to read query params in the routes- req.query
+- How to read dynamics routes- req.params
+
+      Episode-5
+      part-01
+
+- create multiple route Handlers(rH):- play with code
+- learn next() fn & errors along with res.send();
+- do practice:- app.use("/route",rH1,[rH2,rH3],rH4,rH5);
